a194a8e1bdf17a26c66aa2a4a802ae32fadaf277
bunsingit <bunsingit@gmail.com>
2018-09-23 16:17:25 +0700

    commit on cloud

---
+++ resources/views/Home/index.blade.php
@@ -0,0 +1,8 @@
+
+@extends('layouts.cms-layouts')
+@section('content')
+
+    <div class="container">
+        I'm so sorry
+    </div>
+@endsection
---
+++ resources/views/partials/cms-nav.blade.php
@@ -0,0 +1,403 @@
+<nav>
+				<ul>
+					<li class="active">
+						<a href="index.html" title="Dashboard"><i class="fa fa-lg fa-fw fa-home"></i> <span class="menu-item-parent">Dashboard</span></a>
+					</li>
+					<li>
+						<a href="inbox.html"><i class="fa fa-lg fa-fw fa-inbox"></i> <span class="menu-item-parent">Inbox</span><span class="badge pull-right inbox-badge">14</span></a>
+					</li>
+					<li>
+						<a href="#"><i class="fa fa-lg fa-fw fa-bar-chart-o"></i> <span class="menu-item-parent">Graphs</span></a>
+						<ul>
+							<li>
+								<a href="flot.html">Flot Chart</a>
+							</li>
+							<li>
+								<a href="morris.html">Morris Charts</a>
+							</li>
+							<li>
+								<a href="inline-charts.html">Inline Charts</a>
+							</li>
+							<li>
+								<a href="dygraphs.html">Dygraphs</a>
+							</li>
+							<li>
+								<a href="chartjs.html">Chart.js <span class="badge pull-right inbox-badge bg-color-yellow">new</span></a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="#"><i class="fa fa-lg fa-fw fa-table"></i> <span class="menu-item-parent">Tables</span></a>
+						<ul>
+							<li>
+								<a href="table.html">Normal Tables</a>
+							</li>
+							<li>
+								<a href="datatables.html">Data Tables <span class="badge inbox-badge bg-color-greenLight">v1.10</span></a>
+							</li>
+							<li>
+								<a href="jqgrid.html">Jquery Grid</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="#"><i class="fa fa-lg fa-fw fa-pencil-square-o"></i> <span class="menu-item-parent">Forms</span></a>
+						<ul>
+							<li>
+								<a href="form-elements.html">Smart Form Elements</a>
+							</li>
+							<li>
+								<a href="form-templates.html">Smart Form Layouts</a>
+							</li>
+							<li>
+								<a href="validation.html">Smart Form Validation</a>
+							</li>
+							<li>
+								<a href="bootstrap-forms.html">Bootstrap Form Elements</a>
+							</li>
+							<li>
+								<a href="bootstrap-validator.html">Bootstrap Form Validation</a>
+							</li>
+							<li>
+								<a href="plugins.html">Form Plugins</a>
+							</li>
+							<li>
+								<a href="wizard.html">Wizards</a>
+							</li>
+							<li>
+								<a href="other-editors.html">Bootstrap Editors</a>
+							</li>
+							<li>
+								<a href="dropzone.html">Dropzone </a>
+							</li>
+							<li>
+								<a href="image-editor.html">Image Cropping <span class="badge pull-right inbox-badge bg-color-yellow">new</span></a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="#"><i class="fa fa-lg fa-fw fa-desktop"></i> <span class="menu-item-parent">UI Elements</span></a>
+						<ul>
+							<li>
+								<a href="general-elements.html">General Elements</a>
+							</li>
+							<li>
+								<a href="buttons.html">Buttons</a>
+							</li>
+							<li>
+								<a href="#">Icons</a>
+								<ul>
+									<li>
+										<a href="fa.html"><i class="fa fa-plane"></i> Font Awesome</a>
+									</li>	
+									<li>
+										<a href="glyph.html"><i class="glyphicon glyphicon-plane"></i> Glyph Icons</a>
+									</li>	
+									<li>
+										<a href="flags.html"><i class="fa fa-flag"></i> Flags</a>
+									</li>
+								</ul>
+							</li>
+							<li>
+								<a href="grid.html">Grid</a>
+							</li>
+							<li>
+								<a href="treeview.html">Tree View</a>
+							</li>
+							<li>
+								<a href="nestable-list.html">Nestable Lists</a>
+							</li>
+							<li>
+								<a href="jqui.html">JQuery UI</a>
+							</li>
+							<li>
+								<a href="typography.html">Typography</a>
+							</li>
+							<li>
+								<a href="#">Six Level Menu</a>
+								<ul>
+									<li>
+										<a href="#"><i class="fa fa-fw fa-folder-open"></i> Item #2</a>
+										<ul>
+											<li>
+												<a href="#"><i class="fa fa-fw fa-folder-open"></i> Sub #2.1 </a>
+												<ul>
+													<li>
+														<a href="#"><i class="fa fa-fw fa-file-text"></i> Item #2.1.1</a>
+													</li>
+													<li>
+														<a href="#"><i class="fa fa-fw fa-plus"></i> Expand</a>
+														<ul>
+															<li>
+																<a href="#"><i class="fa fa-fw fa-file-text"></i> File</a>
+															</li>
+															<li>
+																<a href="#"><i class="fa fa-fw fa-trash-o"></i> Delete</a></li>
+														</ul>
+													</li>
+												</ul>
+											</li>
+										</ul>
+									</li>
+									<li>
+										<a href="#"><i class="fa fa-fw fa-folder-open"></i> Item #3</a>
+		
+										<ul>
+											<li>
+												<a href="#"><i class="fa fa-fw fa-folder-open"></i> 3ed Level </a>
+												<ul>
+													<li>
+														<a href="#"><i class="fa fa-fw fa-file-text"></i> File</a>
+													</li>
+													<li>
+														<a href="#"><i class="fa fa-fw fa-file-text"></i> File</a>
+													</li>
+												</ul>
+											</li>
+										</ul>
+		
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+				
+					<li>
+						<a href="calendar.html"><i class="fa fa-lg fa-fw fa-calendar"><em>3</em></i> <span class="menu-item-parent">Calendar</span></a>
+					</li>
+					<li>
+						<a href="widgets.html"><i class="fa fa-lg fa-fw fa-list-alt"></i> <span class="menu-item-parent">Widgets</span></a>
+					</li>
+					<li>
+						<a href="#"><i class="fa fa-lg fa-fw fa-puzzle-piece"></i> <span class="menu-item-parent">App Views</span></a>
+						<ul>
+							<li>
+								<a href="projects.html"><i class="fa fa-file-text-o"></i> Projects</a>
+							</li>
+							<li>
+								<a href="blog.html"><i class="fa fa-paragraph"></i> Blog</a>
+							</li>
+							<li>
+								<a href="gallery.html"><i class="fa fa-picture-o"></i> Gallery</a>
+							</li>
+
+							<li>
+								<a href="#"><i class="fa fa-comments"></i> Forum Layout</a>
+								<ul>
+									<li><a href="forum.html">General View</a></li>
+									<li><a href="forum-topic.html">Topic View</a></li>
+									<li><a href="forum-post.html">Post View</a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="profile.html"><i class="fa fa-group"></i> Profile</a>
+							</li>
+							<li>
+								<a href="timeline.html"><i class="fa fa-clock-o"></i> Timeline</a>
+							</li>
+						</ul>		
+					</li>
+					<li>
+						<a href="gmap-xml.html"><i class="fa fa-lg fa-fw fa-map-marker"></i> <span class="menu-item-parent">GMap Skins</span><span class="badge bg-color-greenLight pull-right inbox-badge">9</span></a>
+					</li>
+					<li>
+						<a href="#"><i class="fa fa-lg fa-fw fa-windows"></i> <span class="menu-item-parent">Miscellaneous</span></a>
+						<ul>
+							<li>
+								<a href="http://bootstraphunter.com/smartadmin-landing/" target="_blank">Landing Page <i class="fa fa-external-link"></i></a>
+							</li>
+							<li>
+								<a href="pricing-table.html">Pricing Tables</a>
+							</li>
+							<li>
+								<a href="invoice.html">Invoice</a>
+							</li>
+							<li>
+								<a href="login.html" target="_top">Login</a>
+							</li>
+							<li>
+								<a href="register.html" target="_top">Register</a>
+							</li>
+							<li>
+								<a href="lock.html" target="_top">Locked Screen</a>
+							</li>
+							<li>
+								<a href="error404.html">Error 404</a>
+							</li>
+							<li>
+								<a href="error500.html">Error 500</a>
+							</li>
+							<li>
+								<a href="blank_.html">Blank Page</a>
+							</li>
+							<li>
+								<a href="email-template.html">Email Template</a>
+							</li>
+							<li>
+								<a href="search.html">Search Page</a>
+							</li>
+							<li>
+								<a href="ckeditor.html">CK Editor</a>
+							</li>
+						</ul>
+					</li>
+					<li class="top-menu-invisible">
+						<a href="#"><i class="fa fa-lg fa-fw fa-cube txt-color-blue"></i> <span class="menu-item-parent">SmartAdmin Intel</span></a>
+						<ul>
+							<li>
+								<a href="difver.html"><i class="fa fa-stack-overflow"></i> Different Versions</a>
+							</li>
+							<li>
+								<a href="applayout.html"><i class="fa fa-cube"></i> App Settings</a>
+							</li>
+							<li>
+								<a href="http://bootstraphunter.com/smartadmin/BUGTRACK/track_/documentation/index.html" target="_blank"><i class="fa fa-book"></i> Documentation</a>
+							</li>
+							<li>
+								<a href="http://bootstraphunter.com/smartadmin/BUGTRACK/track_/" target="_blank"><i class="fa fa-bug"></i> Bug Tracker</a>
+							</li>
+						</ul>
+					</li>
+					<li class="chat-users top-menu-invisible">
+						<a href="#"><i class="fa fa-lg fa-fw fa-comment-o"><em class="bg-color-pink flash animated">!</em></i> <span class="menu-item-parent">Smart Chat API <sup>beta</sup></span></a>
+						<ul>
+							<li>
+								<!-- DISPLAY USERS -->
+								<div class="display-users">
+
+									<input class="form-control chat-user-filter" placeholder="Filter" type="text">
+									
+								  	<a href="#" class="usr" 
+									  	data-chat-id="cha1" 
+									  	data-chat-fname="Sadi" 
+									  	data-chat-lname="Orlaf" 
+									  	data-chat-status="busy" 
+									  	data-chat-alertmsg="Sadi Orlaf is in a meeting. Please do not disturb!" 
+									  	data-chat-alertshow="true" 
+									  	data-rel="popover-hover" 
+									  	data-placement="right" 
+									  	data-html="true" 
+									  	data-content="
+											<div class='usr-card'>
+												<img src='img/avatars/5.png' alt='Sadi Orlaf'>
+												<div class='usr-card-content'>
+													<h3>Sadi Orlaf</h3>
+													<p>Marketing Executive</p>
+												</div>
+											</div>
+										"> 
+									  	<i></i>Sadi Orlaf
+								  	</a>
+								  
+									<a href="#" class="usr" 
+										data-chat-id="cha2" 
+									  	data-chat-fname="Jessica" 
+									  	data-chat-lname="Dolof" 
+									  	data-chat-status="online" 
+									  	data-chat-alertmsg="" 
+									  	data-chat-alertshow="false" 
+									  	data-rel="popover-hover" 
+									  	data-placement="right" 
+									  	data-html="true" 
+									  	data-content="
+											<div class='usr-card'>
+												<img src='img/avatars/1.png' alt='Jessica Dolof'>
+												<div class='usr-card-content'>
+													<h3>Jessica Dolof</h3>
+													<p>Sales Administrator</p>
+												</div>
+											</div>
+										"> 
+									  	<i></i>Jessica Dolof
+									</a>
+								  
+									<a href="#" class="usr" 
+									  	data-chat-id="cha3" 
+									  	data-chat-fname="Zekarburg" 
+									  	data-chat-lname="Almandalie" 
+									  	data-chat-status="online" 
+									  	data-rel="popover-hover" 
+									  	data-placement="right" 
+									  	data-html="true" 
+									  	data-content="
+											<div class='usr-card'>
+												<img src='img/avatars/3.png' alt='Zekarburg Almandalie'>
+												<div class='usr-card-content'>
+													<h3>Zekarburg Almandalie</h3>
+													<p>Sales Admin</p>
+												</div>
+											</div>
+										"> 
+									  	<i></i>Zekarburg Almandalie
+									</a>
+								 
+									<a href="#" class="usr" 
+									  	data-chat-id="cha4" 
+									  	data-chat-fname="Barley" 
+									  	data-chat-lname="Krazurkth" 
+									  	data-chat-status="away" 
+									  	data-rel="popover-hover" 
+									  	data-placement="right" 
+									  	data-html="true" 
+									  	data-content="
+											<div class='usr-card'>
+												<img src='img/avatars/4.png' alt='Barley Krazurkth'>
+												<div class='usr-card-content'>
+													<h3>Barley Krazurkth</h3>
+													<p>Sales Director</p>
+												</div>
+											</div>
+										"> 
+									  	<i></i>Barley Krazurkth
+									</a>
+								  
+									<a href="#" class="usr offline" 
+									  	data-chat-id="cha5" 
+									  	data-chat-fname="Farhana" 
+									  	data-chat-lname="Amrin" 
+									  	data-chat-status="incognito" 
+									  	data-rel="popover-hover" 
+									  	data-placement="right" 
+									  	data-html="true" 
+									  	data-content="
+											<div class='usr-card'>
+												<img src='img/avatars/female.png' alt='Farhana Amrin'>
+												<div class='usr-card-content'>
+													<h3>Farhana Amrin</h3>
+													<p>Support Admin <small><i class='fa fa-music'></i> Playing Beethoven Classics</small></p>
+												</div>
+											</div>
+										"> 
+									  	<i></i>Farhana Amrin (offline)
+									</a>
+								  
+									<a href="#" class="usr offline" 
+										data-chat-id="cha6" 
+									  	data-chat-fname="Lezley" 
+									  	data-chat-lname="Jacob" 
+									  	data-chat-status="incognito" 
+									  	data-rel="popover-hover" 
+									  	data-placement="right" 
+									  	data-html="true" 
+									  	data-content="
+											<div class='usr-card'>
+												<img src='img/avatars/male.png' alt='Lezley Jacob'>
+												<div class='usr-card-content'>
+													<h3>Lezley Jacob</h3>
+													<p>Sales Director</p>
+												</div>
+											</div>
+										"> 
+									  	<i></i>Lezley Jacob (offline)
+									</a>
+									
+									<a href="ajax/chat.html" class="btn btn-xs btn-default btn-block sa-chat-learnmore-btn">About the API</a>
+
+								</div>
+								<!-- END DISPLAY USERS -->
+							</li>
+						</ul>	
+					</li>
+					
+				</ul>
+			</nav>
